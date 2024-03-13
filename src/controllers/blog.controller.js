7412afb3cd224d27a43cbecaf130b7731795bfd4
emre durgunlu <emredurgunlu@gmail.com>
2024-03-13 14:58:35 +0300

    Kategori tablosu eklendi

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -2,7 +2,49 @@
 
 require("express-async-errors");
 
-const { BlogPost } = require("../models/blog.model.js");
+const { BlogCategory, BlogPost } = require("../models/blog.model");
+
+module.exports.BlogCategory = {
+  list: async (req, res) => {
+    const data = await BlogCategory.find();
+    res.status(200).send({
+      error: false,
+      data: data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await BlogCategory.create(req.body);
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      data: data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await BlogCategory.find({ _id: req.params.categoryId });
+    res.status(202).send({
+      error: false,
+      data: data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await BlogCategory.updateOne(
+      { _id: req.params.categoryId },
+      req.body
+    );
+    const newdata = await BlogCategory.find({ _id: req.params.categoryId });
+    res.status(202).send({
+      error: false,
+      body: req.body,
+      data: data,
+      newdata: newdata,
+    });
+  },
+  delete: async (req, res) => {
+    const data = await BlogCategory.deleteOne({ _id: req.params.categoryId });
+    res.sendStatus(data.deletedCount >= 1 ? 204 : 404);
+  },
+};
 
 module.exports.BlogPost = {
   list: async (req, res) => {
--- src/models/blog.model.js
+++ src/models/blog.model.js
@@ -2,8 +2,35 @@
 
 const mongoose = require("mongoose");
 
+// BLOG CATEGORY:
+const blogCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    collection: "blogCategory",
+    timestamps: true,
+  }
+);
+
+// 2 tabloyu birbirine bağlarken default ilişki tipi many to one'dır. Buradaki ilişki de many to one'dır.
+// One to one ilişki 2 şekilde yapılabilir ya blogCategoryId: { 'ye unique: true, deriz ya da blogCategorySchema'ya nested bir obje ekleriz 12.satırdaki virgülden sonra relation: {name:{type:..},} gibi
+// Many to many için blogCategoryId: [{...}] array içine alman gerekir
+
+// BLOG POST:
 const blogPostSchema = new mongoose.Schema(
   {
+    blogCategoryId: {
+      type: mongoose.Schema.Types.ObjectId, // ForeignKey, RelationalID
+      ref: "BlogCategory",
+      // buradaki referans ile BlogCategory'e bağlanıyor.
+      // Buraya yazdığın model ismi; BlogCategory: mongoose.model("BlogCategory", blogCategorySchema), bu satırdaki model("BlogCategory" ile aynı olmalı
+      required: true,
+    },
     title: {
       type: String,
       trim: true,
@@ -14,6 +41,10 @@ const blogPostSchema = new mongoose.Schema(
       trim: true,
       required: true,
     },
+    published: {
+      type: Boolean,
+      default: true,
+    },
   },
   {
     collection: "blogPost",
@@ -21,7 +52,7 @@ const blogPostSchema = new mongoose.Schema(
   }
 );
 
-const BlogPostModel = mongoose.model("BlogPost", blogPostSchema);
 module.exports = {
-  BlogPost: BlogPostModel,
+  BlogCategory: mongoose.model("BlogCategory", blogCategorySchema),
+  BlogPost: mongoose.model("BlogPost", blogPostSchema),
 };
