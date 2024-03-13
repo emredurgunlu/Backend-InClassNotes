e42bdb20535aca13ff29c5c9a39bfedcf73cb355
emre durgunlu <emredurgunlu@gmail.com>
2024-03-13 16:28:11 +0300

    passwordEncrypt added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,6 +5,21 @@
 
 const mongoose = require("mongoose");
 
+// Password Encryption:
+// https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+const crypto = require("node:crypto");
+
+const keyCode = process.env?.SECRET_KEY || "write_random_chars_in_here";
+const loopCount = 10_000;
+const charCount = 32; // write 32 for 64
+const encType = "sha512";
+
+const passwordEncrypt = function (password) {
+  return crypto
+    .pbkdf2Sync(password, keyCode, loopCount, charCount, encType)
+    .toString("hex");
+};
+
 // Schema:
 const UserSchema = new mongoose.Schema(
   {
@@ -33,6 +48,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
+      // set: (password) => { return password + '123' },
+      // set: function (password) { return password + '123' },
+      set: (password) => passwordEncrypt(password),
+      // set: passwordEncrypt
     },
 
     firstName: String,
